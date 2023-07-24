c8700616c5481d98a8496da891a16fb7d62a8905
pankajdaundkar <phd27993@gmail.com>
2023-07-24 14:02:15 +0530

    Add project files.

---
+++ Events bind delegate/Bank_event.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_bind_delegate
+{
+    public delegate void EventHandler();
+    public class Bank_event
+    {
+        public event EventHandler Insufficient_balance;
+        public event EventHandler Low_balance;
+        public event EventHandler Zero_balance;
+
+        private int balance;
+
+        public Bank_event()
+        {
+            balance = 10000;
+        }
+
+        public void Credit(int amount)
+        {
+            balance =+ amount;
+           
+        }
+        public void Debit(int amount)
+        {
+            if (balance < amount)
+            {
+                Insufficient_balance();
+            }
+            else if (balance > amount)
+
+            {
+                balance = balance - amount;
+
+                if (balance == 0)
+                {
+                    Zero_balance();
+                }
+                else if (balance < 3000)
+                {
+                    Low_balance();
+                }
+
+            }
+            
+        }
+        public override string ToString()
+        {
+           return$"{balance}";
+        }
+
+    }
+}
---
+++ Events bind delegate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_bind_delegate
+{
+    public class Program
+    {
+        static void Insufficient_balanceMessage()
+        {
+            Console.WriteLine(" opps ! Insufficient Balance");
+        }
+        static void Low_balanceMessage()
+        {
+            Console.WriteLine(" oops ! Low Balance");
+        }
+        static void Zero_Balance()
+        {
+            Console.WriteLine("opps ! zero balance");
+        }
+        static void Main(string[] args)
+        {
+            Bank_event bank_Event = new Bank_event();
+            bank_Event.Insufficient_balance += new EventHandler(Insufficient_balanceMessage);
+            bank_Event.Low_balance += new EventHandler(Low_balanceMessage);
+            bank_Event.Zero_balance += new EventHandler(Zero_Balance);
+
+            Console.WriteLine(bank_Event);
+
+            bank_Event.Debit(400000);
+            Console.WriteLine($"Debited amount is{ bank_Event}");
+            bank_Event.Credit(2000);
+            Console.WriteLine(bank_Event);
+
+
+        }
+
+
+    }
+}
